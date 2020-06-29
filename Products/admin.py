f108812ed8c6aac6bd04cf328c42d9ab5bfbaf44
SUJITH <sujith7994037362@gmail.com>
2020-06-29 09:29:30 +0530

    Add files via upload

---
+++ Products/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import Products,Cart,OrderedItems
+# Register your models here.
+admin.site.register(Products)
+admin.site.register(Cart)
+admin.site.register(OrderedItems)
+
+
+
